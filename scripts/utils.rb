2e4fcef6337f164f1362243a70b586a74ce66038
stevecreedon <steve@creedon.me>
2013-04-23 15:11:26 +0100

    refactored data parsing to an eval statement

--- scripts/utils.rb
+++ scripts/utils.rb
@@ -1,42 +1,7 @@
 def values(values)
-
-  case values.first[0]
-  when "{"
-    return to_h(values)
-  when "["
-    return to_a(values)
-  else
-    puts "value is string: '#{values}'"
-    return values.join(" ")
-  end
-
-end
-
-def to_h(values)
-
-  vals = values.inject({}) do |memo, val|
-    if val.include?(":")
-      bits = val.split(":")
-      raise "expected key:value" unless bits.size == 2
-      memo[bits.first.gsub(/^\{/,"")] = bits.last.gsub(/\}$/,"")
-    end
-    memo
-  end
-
-  puts "value is hash: #{vals}"
-
-end
-
-def to_a(values)
-
-  values[0] = values.first.gsub(/^\[/,"")
-  values[values.size - 1] = values.last.gsub(/\]$/,"") 
-  values.delete_if{|val| val.nil? || val.empty? }
-
-  puts "value is array: #{values}"
-
-  values  
-
+  val = eval(values.join(" "))
+  puts "value is a #{val.class}: #{val}"
+  val 
 end
 
 def hostname_ok?(name)
